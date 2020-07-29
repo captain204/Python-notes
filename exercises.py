8b4e900ae0c4b5e497df0d8f9ec59f0e8a9d60b2
ramkodgreat <nurudeenakindele8@gmail.com>
2020-07-29 07:15:24 +0100

    duplicate list

--- exercises.py
+++ exercises.py
@@ -54,37 +54,97 @@ def secondLargest(arr):
 def maxima(arr):
     max = []
     i = 0
-    size  = len(arr)-1
-    while i < size:
-        if i == 0:
-            if arr[i] > arr[i+1] :
-                value = arr[i]
+    while i < len(arr)-1:
+        if arr[i+1] > arr[i] and arr[i+1] > arr[i+2]:
+            value=arr[i+1]
+        max.append(value)
+        return max
+    i += 1
+
+print(maxima([2,5,1,7,4,1]))
+
+
+
+"""Print all alternate elements of a python list"""
+def alternate(arr):
+    return(arr[1::2])
+
+
+
+
+
+"""Given a list with value 0 or 1 write a program to segregate 0 on the left side and 1 on the right side"""
+def segregate(arr):
+    right = []
+    left = []
+    for value in arr:
+        if value < 1:
+            left.append(value)
         else:
-            if arr[i] > arr[i-1] and arr[i+1]:
-                value = arr[i]
-            max.append(value)
-        i += 1
-    return max
+            right.append(value)
+    return left + right
 
 
-print(maxima([2,5,9,10,6,8,0]))
-
-
-
+"""Given a list of intervals merge all overlapping intervals
+   input:{[1,4],[3,6],[8,10]}
+   output:{[1,6],[8,10]}
+"""
 
 
 
+"""Reversing a list in place"""
+def reverse(arr):
+    start = 0
+    end = len(arr)-1
+    while start < end:
+        temp = arr[start]
+        arr[start] = arr[end]
+        arr[end] = temp
+        start += 1
+        end -=1
+    return arr
+#print(reverse([2,5,1,7,4,1]))
 
 
 
 
 
+"""Segregate zeros ones and twos"""
+
+def segregate_twos(arr):
+    zero = []
+    ones = []
+    twos = []
+    for value in arr:
+        if value < 1:
+            zero.append(value)
+        elif value < 2:
+            ones.append(value)
+        else:
+            twos.append(value)
+    return zero + ones + twos
 
 
+"""Check if a list contains duplicate elements"""
+def duplicate_check(arr):
+    if len(arr) == len(set(arr)):
+        return "Duplicates exist"
+    else:
+        return "List contain no duplicates "
 
 
+"""Find the duplicate elements in a list of size n where each element is in the range 0 to n-1"""
+def duplicate(arr):
+    size = len(arr)
+    repeated = []
+    for i in range(size):
+        k = i + 1
+        for j in range(k,size):
+            if arr[i] == arr[j] and arr[i] not in repeated:
+                repeated.append(arr[i])
+    return repeated
 
-
+print(duplicate([2,5,1,7,4,1,2,4,7]))
 
 
 
